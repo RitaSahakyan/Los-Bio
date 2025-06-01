a435dd2ca726d71bc6af6432aef8ab5e50e942fa
RitaSahakyan <rita.2002.sahakyan@gmail.com>
2025-06-01 12:13:04 +0400

    Component files are added!

---
+++ components/layouts/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header">
+    <div class="header header_top">
+        <div class="header_top-item"></div>
+        <div class="header_top-item"></div>
+        <div class="header_top-item"></div>
+        <div class="header_top-item"></div>
+        <div class="header_top-item"></div>
+    </div>
+    <div class="header header-main"></div>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,9 +4,21 @@ import svgLoader from 'vite-svg-loader'
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  css: ['@/assets/scss/main.scss'],
-  modules: ['@nuxtjs/svgo'],
+  css: [
+    '@/assets/scss/main.scss', 
+  ],
   vite: {
-    plugins: [svgLoader()]
-  }
+    plugins: [svgLoader()],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/scss/variables" as *;
+            @use "@/assets/scss/mixins" as *;
+          `
+        }
+      }
+    }
+  },
+  modules: ['@nuxtjs/svgo'],
 })
